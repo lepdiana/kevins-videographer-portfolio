95c62dd317bb313a9e05cccc0bd2cc230e81747d
Diana Le <lepdiana01@gmail.com>
2019-12-30 18:41:11 -0700

    outlined the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,17 @@ const Home = () => {
     return(
         <div className='homeContainer'>
             <header>
+                Insert picture here to create a paralax on the homepage.
             </header>
+            <div>
+                This section will include a little intro about Kevin. Include a "more info button" that jumps to the information page.
+            </div>
+            <div>
+                This section will include a proud moment. Include a button that jumps the the videography and photography page.
+            </div>
+            <div>
+                This section will include services that Kevin would be happy to create. Also include a button that jumps to the contact page.
+            </div>
         </div>
     )
 }
