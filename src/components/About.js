65450b54e594ec34c8640ae91e2aa7677400ad0a
Diana Le <lepdiana01@gmail.com>
2020-01-09 00:07:12 -0700

    fixed home page css

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <header className='header-paralax'>
                 <h1>About</h1>
             </header>
-            <main>
+            <main className='aboutMain'>
                 <div className='aboutContainer'>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi quo possimus ipsa magni placeat. Maxime dolorem obcaecati rem fugit illum, dolores saepe quibusdam, illo quos velit explicabo architecto praesentium! Provident! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ullam quia dolore, nulla iusto itaque molestias optio distinctio delectus placeat odio ipsam animi repellat veritatis voluptatem deserunt? Distinctio, est modi! </p>
                     <img src='https://images.unsplash.com/photo-1477160739634-171ff0343882?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1387&q=80' alt='cameraGear' className='aboutImage'/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,8 @@ const Home = () => {
             <header>
                 <h1>Kevin Nguyen</h1>
             </header>
-            <main>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus beatae perferendis, reprehenderit doloremque dolor repellendus nesciunt, sunt tempore eum tenetur magni rem nihil maxime labore natus accusamus aspernatur veritatis molestias? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum molestias id perspiciatis dolorum et necessitatibus alias, numquam placeat! Quo voluptate laboriosam doloremque earum dolorum ut repellendus perferendis facere natus quod.
+            <main className='homeMain'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus beatae perferendis, reprehenderit doloremque dolor repellendus nesciunt, sunt tempore eum tenetur magni rem nihil maxime labore natus accusamus aspernatur veritatis molestias? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum molestias id perspiciatis dolorum et necessitatibus alias, numquam placeat! Quo voluptate laboriosam doloremque earum dolorum ut repellendus perferendis facere natus quod.</p>
             </main>
             <div>
                 <HomeNavbar />
