bbed27c562c5f3416758b4ae24caa7b921f1f4f7
Diana Le <lepdiana01@gmail.com>
2020-01-09 23:37:50 -0700

    added in class name to initialize the styling portion

--- src/components/Videography.js
+++ src/components/Videography.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GeneralNavbar from './GeneralNavbar.js';
+import '../styles/videography.css'
 
 const Videography = () => {
     return(
@@ -7,23 +8,23 @@ const Videography = () => {
             <div className='generalNavbar'>
                 <GeneralNavbar />
             </div>
-            <header>
-                <h1>Videography</h1>
+            <header className='videographyHeader'>
+                <h1 className='videographyTitle'>Videography</h1>
             </header>
-            <main>
-                <div>
+            <main className='videographyMain'>
+                <div className='videographyProject'>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel similique perferendis, delectus quasi enim pariatur in dolores labore aliquam nemo harum fugiat cumque ea atque sint iste placeat sapiente laborum?</p>
                     <div>
                         ***video here***
                     </div>
                 </div>
-                <div>
+                <div className='videographyProject'>
                     <div>
                         ***video here***
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt dignissimos quisquam iure, accusantium aperiam repellat sapiente tenetur fuga inventore atque sunt iusto reiciendis vitae, possimus doloribus nam ullam eveniet. Hic.</p>
                 </div>
-                <div>
+                <div className='videographyProject'>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel similique perferendis, delectus quasi enim pariatur in dolores labore aliquam nemo harum fugiat cumque ea atque sint iste placeat sapiente laborum?</p>
                     <div>
                         ***video here***
